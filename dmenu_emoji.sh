87910dce5c3bbf31bf0af356b0e3a24f00744773
Subado <redcatgamer1980@gmail.com>
2022-12-28 21:47:22 +0400

    notify-send not executed if nothing copied

--- dmenu_emoji.sh
+++ dmenu_emoji.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 grep -v "#" ~/scripts/emoji_list.txt | dmenu -i -l 20 -fn "Hack Nerd Font-15" | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
-notify-send "$(xclip -o -selection clipboard) copied to clipboard."
+[[ -n $(xclip -o -selection clipboard) ]] && notify-send "$(xclip -o -selection clipboard) copied to clipboard."
